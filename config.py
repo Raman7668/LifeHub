366c56b30b173c88fff78f77ae68ddcec10885ed
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-12-15 14:30:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =22475741
+api_hash = "1a217be71a0225e0a678af286c211f8a"
+bot_token = "7347353498:AAGgNzcVVGcEUoZSYAhghggmKpgX8_XUYvs "
+auth_users = "6562642609"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "6562642609"
 owner_users = [int(num) for num in osowner_users.split(",")]
